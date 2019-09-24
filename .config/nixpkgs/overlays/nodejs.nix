830777930a016543a822b08e88cfed15d1a3a02b
Franz Thoma <franz.thoma@tngtech.com>
2019-09-24 19:25:28 +0200

    fixup! Backport nodejs-8_x

--- .config/nixpkgs/overlays/nodejs.nix
+++ .config/nixpkgs/overlays/nodejs.nix
@@ -7,4 +7,5 @@ self: super: {
             sha256 = "04h5ddshz2id7jp2v1b7klj4x38ssf1zhbf296ppz5idvyn91hfq";
         };
     });
+    yarn-8_x = super.yarn.override { nodejs = self.nodejs-8_x; };
 }
